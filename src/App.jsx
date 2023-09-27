cbc363ad91902833d44becf7e4461f72aa676c6b
UsuarioM <espacioguriats@gmail.com>
2023-09-27 13:52:25 +0200

    Reloj

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countdown from './components/Countdown/Countdown'
+import DigitalClock from './components/DigitalClock/DigitalClock'
+
+const App = () => {
+
+
+  return (
+    <>
+     <DigitalClock/>
+     <Countdown/>
+    </>
+  )
+}
+
+export default App
